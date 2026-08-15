c605de1fe4feff8ae134d8af4ee42c80e65eda28
Alastor Kaneki <170276077+Alastor-Kaneki@users.noreply.github.com>
2026-08-15 00:42:57 -0400

    Add primary color compat helper

---
+++ MinecraftHub/app/src/main/java/dev/alastorkaneki/minecrafthub/PrimaryColorCompat.java
@@ -0,0 +1,6 @@
+package dev.alastorkaneki.minecrafthub;
+
+final class PrimaryColorCompat {
+    private PrimaryColorCompat() {}
+    static int attr() { return androidx.appcompat.R.attr.colorPrimary; }
+}
